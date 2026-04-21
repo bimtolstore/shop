9266a598afdbf64c569b285c54e563a4c57ac335
bimtolstore <akunhutaoayato22@gmail.com>
2026-04-21 18:06:55 +0700

    Add files via upload

---
+++ biresto/menuhariini.js
@@ -0,0 +1,44 @@
+const menuReady = [
+    { 
+        name: "Nasi Uduk Lengkap + Telur Balado", 
+        price: 12000, 
+        stock: 10, 
+        description: "Nasi uduk gurih, telur balado, tempe orek, bihun, dan sambal.",
+        img: "https://cdn1-production-images-kly.akamaized.net/eXiPeL9cUcC48oCbZLNJQxTGFEY=/1x60:700x454/1280x720/filters:quality(75):strip_icc()/kly-media-production/medias/3264167/original/045475800_1602402228-1212646998.jpg" 
+    },
+    { 
+        name: "Nasi Kuning Lengkap + Telur Balado", 
+        price: 12000, 
+        stock: 10,
+        description: "Nasi kuning wangi, telur balado, tempe orek, bihun dan sambal",
+        img: "https://cdn.grid.id/crop/0x0:0x0/700x465/smart/filters:format(webp):quality(100)/photo/2025/12/16/cara-masak-nasi-kuning-rice-cook-20251216115928.jpg" 
+    },
+    { 
+        name: "Nasi Ayam Geprek Sambel Ijo", 
+        price: 12000, 
+        stock: 10, 
+        description: "Ayam goreng krispi yang digeprek dengan sambal ijo khas.",
+        img: "https://d1d8o7q9jg8pjk.cloudfront.net/p/lg_68a4617b6acd9.jpg" 
+    },
+    { 
+        name: "Tahu Isi", 
+        price: 3000, 
+        stock: 20, 
+        description: "Tahu goreng renyah dengan isian sayur gurih dan pedas.",
+        img: "https://akcdn.detik.net.id/community/media/visual/2023/10/19/resep-gehu-tahu-goremg-pedas_43.jpeg?w=600&q=90" 
+    },
+    { 
+        name: "Bakwan", 
+        price: 2000, 
+        stock: 20, 
+        description: "Gorengan sayur (bala-bala) yang garing di luar.",
+        img: "https://i0.wp.com/resepkoki.id/wp-content/uploads/2019/11/Bala-Bala.jpg?fit=500%2C667&ssl=1" 
+    },
+    { 
+        name: "Es Teh Manis", 
+        price: 5000, 
+        stock: 20, 
+        description: "Teh manis segar dengan es batu.",
+        img: "https://images.unsplash.com/photo-1556679343-c7306c1976bc?w=200" 
+    }
+];
---
+++ biresto/menupreorder.js
@@ -0,0 +1,10 @@
+// Data Menu Pre-Order (Untuk Besok)
+const menuPO = [
+    { 
+        name: "Pre-Nasi Ayam Penyet", 
+        price: 12000, 
+        stock: 5, 
+        description: "Menu PO besok: Nasi, ayam penyet sambal merah, lalapan.",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Ayam_geprek.png/500px-Ayam_geprek.png" 
+    }
+];
