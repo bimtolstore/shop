1c307e58dab6a1444b30354d841d3710ab0c1398
bimtolstore <akunhutaoayato22@gmail.com>
2026-06-16 17:39:25 +0700

    Add files via upload

---
+++ archi-deposito/hasna.js
@@ -0,0 +1,159 @@
+const hasnaData = {
+    "version": 1.0,
+    "user": {
+        "name": "Archi",
+        "accountType": "Premium"
+    },
+    
+    "kantong": [
+        { 
+            "id": "k1", 
+            "name": "Kantong Biasa", 
+            "balance": 10145, 
+            "classification": "primer", 
+            "icon": "fa-wallet", 
+            "color": "#0081a7" 
+        },
+        { 
+            id: "k2", 
+            name: "Uang Jajan", 
+            balance: 0, 
+            classification: "pengeluaran", 
+            icon: "fa-burger", 
+            color: "#e63946" 
+        },
+        { 
+            id: "k3", 
+            name: "Tabungan Liburan (+2% Pertahun)", 
+            balance: 0, 
+            classification: "tabungan", 
+            icon: "fa-plane", 
+            color: "#2a9d8f" 
+        },
+        { 
+            id: "k4", 
+            name: "Tabungan Jangka Panjang (+2,25% Pertahun)", 
+            balance: 0, 
+            classification: "tabungan", 
+            icon: "fa-piggy-bank", 
+            color: "#f4a261" 
+        },
+        { 
+            id: "k5", 
+            name: "Reksa Dana & Pasar Uang (+3% Pertahun)", 
+            balance: 0, 
+            classification: "investasi", 
+            icon: "fa-chart-line", 
+            color: "#9b5de5" 
+        },
+        { 
+            id: "k6", 
+            name: "US Dollar", 
+            balance: 0, 
+            classification: "USD", 
+            icon: "fa-dollar", 
+            color: "#9b5de5" 
+        }
+    ],
+    riwayat: [
+        {
+            id: "t1", 
+            title: "QRIS Ke PT Tokopedia", 
+            pocket: "Kantong Utama", 
+            type: "out", 
+            amount: 26300, 
+            date: "16 Jun 2026, 13:05", 
+            icon: "fa-arrow-up" 
+        },
+        {
+            id: "t2", 
+            title: "QRIS Ke PT Tokopedia", 
+            pocket: "Kantong Utama", 
+            type: "out", 
+            amount: 17000, 
+            date: "12 Jun 2026, 06:16", 
+            icon: "fa-arrow-up" 
+        },
+        {
+            id: "t3", 
+            title: "Bunga",
+            pocket: "Kantong Utama", 
+            type: "in", 
+            amount: 25, 
+            date: "12 Jun 2026, 10:46", 
+            icon: "fa-plus" 
+        },
+        {
+            id: "t4", 
+            title: "Deposito via QRIS", 
+            pocket: "Kantong Utama", 
+            type: "in", 
+            amount: 25000, 
+            date: "9 Jun 2026, 10:45", 
+            icon: "fa-arrow-down" 
+        },
+        {
+            id: "t5", 
+            title: "QRIS Ke PT Tokopedia", 
+            pocket: "Kantong Utama", 
+            type: "out", 
+            amount: 80700, 
+            date: "9 Jun 2026, 07:24", 
+            icon: "fa-arrow-up" 
+        },
+        {
+            id: "t6", 
+            title: "Biaya Transfer", 
+            pocket: "Kantong Utama", 
+            type: "out", 
+            amount: 1000, 
+            date: "4 Jun 2026, 14:36", 
+            icon: "fa-minus" 
+        },
+        {
+            id: "t7", 
+            title: "Transfer Bank Mandiri", 
+            pocket: "Kantong Utama", 
+            type: "out", 
+            amount: 10000, 
+            date: "4 Jun 2026, 14:36", 
+            icon: "fa-arrow-up" 
+        },
+        {
+            id: "t8", 
+            title: "Bunga", 
+            pocket: "Kantong Utama", 
+            type: "in", 
+            amount: 100, 
+            date: "3 Jun 2026, 04:46", 
+            icon: "fa-plus" 
+        },
+        {
+            id: "t9", 
+            title: "Deposito via QRIS", 
+            pocket: "Kantong Utama", 
+            type: "in", 
+            amount: 100000, 
+            date: "2 Jun 2026, 21:07", 
+            icon: "fa-arrow-down" 
+        },
+        {
+            id: "t10", 
+            title: "Bunga", 
+            pocket: "Kantong Utama", 
+            type: "in", 
+            amount: 20, 
+            date: "30 Mei 2026, 18:52", 
+            icon: "fa-plus" 
+        },
+        {
+            id: "t11", 
+            title: "Deposito via QRIS", 
+            pocket: "Kantong Utama", 
+            type: "in", 
+            amount: 20000, 
+            date: "30 Mei 2026, 18:52", 
+            icon: "fa-arrow-down" 
+        }
+    ]
+};
